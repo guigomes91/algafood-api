fd9417392213f6f9e00466421ca0a87d5769a25d
guigomes91 <guilherme.gomes91@outlook.com>
2024-07-06 12:59:08 -0300

    Métodos de callback

--- src/main/java/com/algaworks/algafood/ServiceConfig.java
+++ src/main/java/com/algaworks/algafood/ServiceConfig.java
@@ -5,9 +5,9 @@ import com.algaworks.algafood.service.AtivacaoClienteService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class ServiceConfig {
-    @Bean
+    @Bean(initMethod = "init", destroyMethod = "destroy")
     public AtivacaoClienteService ativacaoClienteService(Notificador notificador) {
         return new AtivacaoClienteService();
     }
--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -5,15 +5,27 @@ import com.algaworks.algafood.notificacao.ATipoNotificador;
 import com.algaworks.algafood.notificacao.Notificador;
 import com.algaworks.algafood.notificacao.TipoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//@Component
 public class AtivacaoClienteService {
 
     @ATipoNotificador(TipoNotificador.URGENTE)
     @Autowired
     private Notificador notificador;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
