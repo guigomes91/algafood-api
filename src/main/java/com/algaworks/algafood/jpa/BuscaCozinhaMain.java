b82a8e6086300a143fa93604acfdd4a48b520c76
guigomes91 <guilherme.gomes91@outlook.com>
2024-07-13 08:07:13 -0300

    Busca por id

--- src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/BuscaCozinhaMain.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.List;
 
-public class InclusaoCozinhaMain {
+public class BuscaCozinhaMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -16,16 +16,7 @@ public class InclusaoCozinhaMain {
                 .run(args);
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Japonesa");
-
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Brasileira");
-
-        cozinha1 = cadastroCozinha.adicionar(cozinha1);
-        cozinha2 = cadastroCozinha.adicionar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+        System.out.println(cozinha.getNome());
     }
 }
