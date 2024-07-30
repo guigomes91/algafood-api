967a50d5b0dd8584df207f7dd832d95fdfd239cf
guigomes91 <guilherme.gomes91@outlook.com>
2024-07-29 21:15:01 -0300

    Method merge

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -91,9 +91,6 @@ public class RestauranteController {
             field.setAccessible(true);
 
             Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-            System.out.println(nomePropriedade + ":" + valorPropriedade + " = " + novoValor);
-            
             ReflectionUtils.setField(field, restauranteDestino, novoValor);
         });
     }
