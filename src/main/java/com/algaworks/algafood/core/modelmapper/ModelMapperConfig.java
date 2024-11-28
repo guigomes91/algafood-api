c50f6e8978c0e060e468078d9b8384f17f545cb8
guigomes91 <guilherme.gomes91@outlook.com>
2024-11-27 21:07:48 -0300

    modelando endereco no restaurante model

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +14,14 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         var modelMapper = new ModelMapper();
-        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+//        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value)
+        );
 
         return modelMapper;
     }
