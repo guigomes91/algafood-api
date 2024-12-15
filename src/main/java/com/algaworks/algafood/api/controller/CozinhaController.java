f2297c5ac3d0d999cfe957f0f76810509c1a9c6a
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-15 16:46:41 -0300

    add paginacao em cozinhas

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -8,6 +8,9 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +28,13 @@ public class CozinhaController {
     private final CozinhaModelDisassembler cozinhaModelDisassembler;
 
     @GetMapping
-    public List<CozinhaModel> listar() {
-        return cozinhaModelAssembler.toCollectionModel(cozinhaRepository.findAll());
+    public Page<CozinhaModel> listar(Pageable pageable) {
+        final var cozinhas = cozinhaRepository.findAll(pageable);
+
+        var cozinhasModel = cozinhaModelAssembler.toCollectionModel(cozinhas.getContent());
+
+        var cozinhasModelPage = new PageImpl<>(cozinhasModel, pageable, cozinhas.getTotalElements());
+        return cozinhasModelPage;
     }
 
     @GetMapping(value = "/{id}")
