c7452a6709203299eb026936e53d611ff4249ba7
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-18 22:57:56 -0300

    filtros na consulta de vendas diarias

--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.infrastructure.service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.model.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.service.VendaQueryService;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -25,12 +28,34 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 
         var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
 
+        var predicates = new ArrayList<Predicate>();
+
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+        if (vendaDiariaFilter.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), vendaDiariaFilter.getRestauranteId()));
+        }
+
+        if (vendaDiariaFilter.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(
+                    root.get("dataCriacao"),
+                    vendaDiariaFilter.getDataCriacaoInicio()
+            ));
+        }
+
+        if (vendaDiariaFilter.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(
+                    root.get("dataCriacao"),
+                    vendaDiariaFilter.getDataCriacaoFim()
+            ));
+        }
+
         var selection = builder.construct(VendaDiaria.class,
                 functionDateDataCriacao,
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal")));
 
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateDataCriacao);
 
         return entityManager.createQuery(query).getResultList();
