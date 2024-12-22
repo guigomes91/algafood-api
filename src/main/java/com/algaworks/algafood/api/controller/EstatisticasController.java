c9a846ca08ff5be25effa228d02cdc5b69f6b0d2
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-22 14:31:31 -0300

    emissao de relatorio accept pdf na api

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,7 +3,11 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.model.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +21,26 @@ import java.util.List;
 public class EstatisticasController {
 
     private final VendaQueryService vendaQueryService;
+    private final VendaReportService vendaReportService;
 
     @GetMapping("/vendas-diarias")
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter vendaDiariaFilter,
                                                     @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
         return vendaQueryService.consultarVendasDiarias(vendaDiariaFilter, timeOffset);
+    }
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+                                                            @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+
+        byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .headers(headers)
+                .body(bytesPdf);
     }
 }
