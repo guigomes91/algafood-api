2a82e6d29ad06b8b5ad08dbc62dfdb383eab63fe
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-23 22:26:21 -0300

    controller para upload de foto

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -0,0 +1,48 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.assembler.ProdutoModelAssembler;
+import com.algaworks.algafood.api.assembler.ProdutoModelDisassembler;
+import com.algaworks.algafood.api.model.ProdutoModel;
+import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.api.model.input.ProdutoInput;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.repository.ProdutoRepository;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
+public class RestauranteProdutoFotoController {
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void atualizarFoto(@PathVariable Long restauranteId,
+                              @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+
+        var nomeArquivo = UUID.randomUUID().toString()
+                + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+
+        var arquivoFoto = Path.of("/Users/guilh/OneDrive/Imagens/spi", nomeArquivo);
+
+        System.out.println(fotoProdutoInput.getDescricao());
+        System.out.println(arquivoFoto);
+        System.out.println(fotoProdutoInput.getArquivo().getContentType());
+
+        try {
+            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
