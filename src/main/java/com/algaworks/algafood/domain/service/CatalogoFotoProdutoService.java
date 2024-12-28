bf83bf9c732ba8888734d2f2c8a5944af761d4e5
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-27 21:42:27 -0300

    local storage em disco

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.InputStream;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -14,10 +15,13 @@ public class CatalogoFotoProdutoService {
 
     private final ProdutoRepository produtoRepository;
 
+    private final FotoStorageService fotoStorage;
+
     @Transactional
-    public FotoProduto salvar(FotoProduto foto) {
+    public FotoProduto salvar(FotoProduto foto, InputStream dadosArquivo) {
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
+        String nomeNovoArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
 
         Optional<FotoProduto> fotoExistente = produtoRepository
                 .findFotoById(restauranteId, produtoId);
@@ -26,6 +30,17 @@ public class CatalogoFotoProdutoService {
             produtoRepository.delete(fotoExistente.get());
         }
 
-        return produtoRepository.save(foto);
+        foto.setNomeArquivo(nomeNovoArquivo);
+        foto =  produtoRepository.save(foto);
+        produtoRepository.flush();
+
+        FotoStorageService.NovaFoto novaFoto = FotoStorageService.NovaFoto.builder()
+                .nomeAquivo(foto.getNomeArquivo())
+                .inputStream(dadosArquivo)
+                .build();
+
+        fotoStorage.armazenar(novaFoto);
+
+        return foto;
     }
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+import java.util.UUID;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+
+    default String gerarNomeArquivo(String nomeOriginal) {
+        return UUID.randomUUID().toString() + "_" + nomeOriginal;
+    }
+
+    @Builder
+    @Getter
+    class NovaFoto {
+
+        private String nomeAquivo;
+        private InputStream inputStream;
+    }
+}
