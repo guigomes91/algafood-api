b5e3d82289f054af3f6dd5bdd2272769e6854648
guigomes91 <guilherme.gomes91@outlook.com>
2024-12-29 14:56:05 -0300

    remocao de fotos local

--- src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CatalogoFotoProdutoService.java
@@ -21,11 +21,15 @@ public class CatalogoFotoProdutoService {
         Long restauranteId = foto.getRestauranteId();
         Long produtoId = foto.getProduto().getId();
         String nomeNovoArquivo = fotoStorage.gerarNomeArquivo(foto.getNomeArquivo());
+        String nomeArquivoExistente = null;
 
         Optional<FotoProduto> fotoExistente = produtoRepository
                 .findFotoById(restauranteId, produtoId);
 
-        fotoExistente.ifPresent(produtoRepository::delete);
+        if (fotoExistente.isPresent()) {
+            nomeArquivoExistente = fotoExistente.get().getNomeArquivo();
+            produtoRepository.delete(fotoExistente.get());
+        }
 
         foto.setNomeArquivo(nomeNovoArquivo);
         foto =  produtoRepository.save(foto);
@@ -36,7 +40,7 @@ public class CatalogoFotoProdutoService {
                 .inputStream(dadosArquivo)
                 .build();
 
-        fotoStorage.armazenar(novaFoto);
+        fotoStorage.substituir(nomeArquivoExistente, novaFoto);
 
         return foto;
     }
