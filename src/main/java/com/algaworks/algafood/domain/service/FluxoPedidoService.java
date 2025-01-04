8aa91789c070c2c0e9832e351f494cc8153190b6
guigomes91 <guilherme.gomes91@outlook.com>
2025-01-04 18:04:17 -0300

    smtp para email

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,23 +1,27 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.domain.exception.NegocioException;
-import com.algaworks.algafood.domain.model.StatusPedido;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.OffsetDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class FluxoPedidoService {
 
     private final EmissaoPedidoService emissaoPedidoService;
+    private final EnvioEmailService envioEmailService;
 
     @Transactional
     public void confirmar(String codigoPedido) {
         var pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
+
+        var mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+        envioEmailService.enviar(mensagem);
     }
 
     @Transactional
