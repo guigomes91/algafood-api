f1f5c5e998e875e881ac3c45c96732814e321469
guigomes91 <guilherme.gomes91@outlook.com>
2025-02-13 20:01:09 -0300

    Habilitando globalmente CORS na API

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/restaurantes")
