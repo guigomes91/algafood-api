a60df330e4cf8514255c1ce26b8c8d86ad80e953
guigomes91 <guilherme.gomes91@outlook.com>
2025-04-01 21:56:15 -0300

    logs e ajustes em e stado controller

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -4,17 +4,19 @@ import com.algaworks.algafood.api.assembler.EstadoInputDisassembler;
 import com.algaworks.algafood.api.assembler.EstadoModelAssembler;
 import com.algaworks.algafood.api.model.EstadoModel;
 import com.algaworks.algafood.api.model.input.EstadoInput;
-import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 import com.algaworks.algafood.domain.service.CadastroEstadoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jfree.util.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/estados")
@@ -27,6 +29,7 @@ public class EstadoController {
 
     @GetMapping
     public List<EstadoModel> listar() {
+        Log.info("Listando estados...");
         return estadoModelAssembler.toCollectionModel(estadoRepository.findAll());
     }
 
